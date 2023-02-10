a26bf96f3b9b87f7af101782b82a8a7b0d8b6abc
Elio1995 <elio.mehmeti@kineton.al>
2023-02-10 15:16:03 +0100

    Removed fetch from app

--- App.tsx
+++ App.tsx
@@ -9,19 +9,6 @@ import {store} from './src/Redux/store';
 
 const Stack = createStackNavigator();
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '979a36c7camsh7f2351cd4644296p1a673bjsn0af29bb0089f',
-    'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com',
-  },
-};
-
-fetch('https://sportscore1.p.rapidapi.com/sports/1/leagues', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
 function App() {
   return (
     <NavigationContainer>
